75b7f3b4dc7759e5cab2983e56248525bfafd9c0
unknown <khoirulhudaid@gmail.com>
2019-02-12 21:01:39 +0700

    laravel-minggu-ke-4

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,27 @@
+@extends('templates.default')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                <table class="table table-boredered">
+                    <tr class="tr">
+                        <th>No</th>
+                        <th>judul</th>
+                        <th>konten</th>
+                        <th>aksi</th>
+                    </tr>
+
+                    @foreach (articles as article)
+                    <tr>
+                        <td>{{$articles as $article}}</td>
+                        <td>{{$article->title}}</td>
+                        <td>{{$article->content}}</td>
+                        <td><a href="{{route('article.edit - $article')}}" class="btn btn-primary">edit</a></td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function() {
+   return view('admin', [
+       'title' => 'Artikel 1'
+   ]);
+});
+
+Route::get('/test', function() {
+    return view('test', [
+        'title' => 'Artikel Test'
+    ]);
+ });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('article/create', 'ArticleController@create')->name('article.create')->middleware('auth');
+Route::post('article', 'ArticleController@store')->name('article.store');
+Route::get('article/{id}/edit','ArticleController@edit')->name('article.edit')->middleware('auth');
+Route::put('article/{id}/update', 'ArticleController@update')->name('article.update');
+Route::get('article','ArticleController@index')->name('article.index');
